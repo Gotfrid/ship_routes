e5fb2028e7838daa18a40b0f81b6b15859a85c9d
Pavel <paveldemin@me.com>
2021-06-19 15:14:31 +0300

    bug fix had to implement reactive values

--- server.R
+++ server.R
@@ -1,12 +1,17 @@
 function(input, output, session) {
     
+    # init reactive values
+    rv <- reactiveValues()
+    
     # create and update select inputs
     dropdownSelect_server("dropdown_menus")
     
     # obtain all useful data for user input
-    data_to_display <- reactive({
-        get_data(type = input$`dropdown_menus-type_selection`,
-                 name = input$`dropdown_menus-name_selection`)
+    observeEvent(input$`dropdown_menus-name_selection`, {
+        rv$data_to_display <- get_data(
+            type = input$`dropdown_menus-type_selection`,
+            name = input$`dropdown_menus-name_selection`
+        )
     })
     
     # create the map
@@ -14,19 +19,20 @@ function(input, output, session) {
 
     # update data on the map
     observe({
-        map_proxy("map-map", data_to_display())
+        print(rv$data_to_display)
+        map_proxy("map-map", rv$data_to_display)
     })
     
     # update infographics
     observe({
-        req(data_to_display())
+        req(rv$data_to_display)
         
         # post information in infobox
         info_server(id = "info_box", 
-                    info_data = data_to_display())
+                    info_data = rv$data_to_display)
         
         # draw a plot
-        plot_server("plot", data_to_display())
+        plot_server("plot", rv$data_to_display)
     })
 
 }
