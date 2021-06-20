43ff3062beabf1a735a50fcf3f0f9a2b902dafad
Pavel <paveldemin@me.com>
2021-06-20 23:10:14 +0300

    bug fix

--- global.R
+++ global.R
@@ -13,18 +13,18 @@ library(echarts4r) # interactive viz
 # prepare initial select options
 vessel_by_type <- readRDS("data/vessel_by_type.RDS")
 type_options <- unique(vessel_by_type$ship_type)
-name_options <- vessel_by_type[vessel_by_type$type == "Tanker", "ship_name"]
+name_options <- vessel_by_type[vessel_by_type$ship_type == "Tanker", "ship_name"]
 
 # page grid
 myGridTemplate <- grid_template(
   default = list(
     areas = rbind(
-      c("title", "title", "title", "title"),
-      c("selection", "selection", "vertical_separation", "blank_space"),
+      c("title", "title", "title", "help"),
       c("map", "map", "vertical_separation", "info_box"),
-      c("map", "map", "vertical_separation", "infographic")
+      c("map", "map", "vertical_separation", "infographic"),
+      c("selection", "selection", "vertical_separation", "blank_space")
     ),
-    cols_width = c("320px", "300px", "20px", "320px"),
-    rows_height = c("50px", "80px", "210px", "auto")
+    cols_width = c("320px", "3fr", "20px", "1fr"),
+    rows_height = c("100px", "235px", "360px", "80px")
   )
 )
